f9bbe80f2b9ac61372a5bf6c3dcf6277c37bce5e
zzarbttoo <qodbwls064@gmail.com>
2019-09-27 12:43:17 +0900

    xml file

--- Blueberry/app/src/main/java/ssu/cheesecake/blueberry/ReCheck2.java
+++ Blueberry/app/src/main/java/ssu/cheesecake/blueberry/ReCheck2.java
@@ -1,6 +1,7 @@
 package ssu.cheesecake.blueberry;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,19 @@ public class ReCheck2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_re_check2);
+
+        EditText editText1=findViewById(R.id.name_confirm);
+        editText1.setText("The text1");
+
+        EditText editText2=findViewById(R.id.phone_confirm);
+        editText2.setText("The text2");
+
+        EditText editText3=findViewById(R.id.mail_confirm);
+        editText3.setText("The text3");
+
+        EditText editText4=findViewById(R.id.company_confirm);
+        editText4.setText("The text4");
     }
+
+
 }
--- Blueberry/app/src/main/java/ssu/cheesecake/blueberry/Recheck.java
+++ Blueberry/app/src/main/java/ssu/cheesecake/blueberry/Recheck.java
@@ -1,61 +1,20 @@
 package ssu.cheesecake.blueberry;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Recheck extends AppCompatActivity{
+public class Recheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            final String[] name={"1번", "2번"}; //이거는 머신러닝에서 값에 따라 달라지게 하려함
-            final String[] phone_number={"010","0103"};//이거도 정수로 해야하나 문자인가 모르겠다
-            final String[] company={"회사1","회사2"};
-            final String[] address={"주소1", "주소2"};
-
-            final Spinner spin1=findViewById(R.id.spinner1);
-            final Spinner spin2=findViewById(R.id.spinner2);
-            final Spinner spin3=findViewById(R.id.spinner3);
-            final Spinner spin4=findViewById(R.id.spinner4);
-
-            ArrayAdapter adapter1= new ArrayAdapter(getApplicationContext(),R.layout.spin, name);
-            adapter1.setDropDownViewResource(R.layout.spin_dripdown);
-            spin1.setAdapter(adapter1);
-
-            ArrayAdapter adapter2= new ArrayAdapter(getApplicationContext(), R.layout.spin, phone_number);
-            adapter2.setDropDownViewResource(R.layout.spin_dripdown);
-            spin2.setAdapter(adapter2);
-
-            ArrayAdapter adapter3= new ArrayAdapter(getApplicationContext(),R.layout.spin, company);
-            adapter3.setDropDownViewResource(R.layout.spin_dripdown);
-            spin3.setAdapter(adapter3);
-
-            ArrayAdapter adapter4= new ArrayAdapter(getApplicationContext(),R.layout.spin,address);
-            adapter4.setDropDownViewResource(R.layout.spin_dripdown);
-            spin4.setAdapter(adapter4);
-
-            Button button=findViewById(R.id.finishButton1);
-            button.setOnClickListener(new View.OnClickListener(){
-
-                @Override
-                public void onClick(View v){
-                    Intent intent = new Intent(getApplicationContext(),ReCheck2.class);
-                    startActivity(intent);
-                }
-            });
-
-        }
 
 
+    }
 
 
 }
