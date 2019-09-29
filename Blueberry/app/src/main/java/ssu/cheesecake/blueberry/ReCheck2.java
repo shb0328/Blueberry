15b11e34a8167502845826573ba720f27bf121fc
zzarbttoo <qodbwls064@gmail.com>
2019-09-29 22:16:58 +0900

    Recheck1 still not working

--- Blueberry/app/src/main/java/ssu/cheesecake/blueberry/ReCheck2.java
+++ Blueberry/app/src/main/java/ssu/cheesecake/blueberry/ReCheck2.java
@@ -2,6 +2,7 @@ package ssu.cheesecake.blueberry;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,16 +14,31 @@ public class ReCheck2 extends AppCompatActivity {
         setContentView(R.layout.activity_re_check2);
 
         EditText editText1=findViewById(R.id.name_confirm);
-        editText1.setText("The text1");
+        editText1.setText(getResources().getString(R.string.recheck2_name1));
+
+        TextView name_confirm2 = findViewById(R.id.name_confirm2);
+        name_confirm2.setText(getResources().getString(R.string.recheck2_name2));
 
         EditText editText2=findViewById(R.id.phone_confirm);
-        editText2.setText("The text2");
+        editText2.setText(getResources().getString(R.string.recheck2_phone1));
+
+        TextView phone_confirm2= findViewById(R.id.phone_confirm2);
+        phone_confirm2.setText(getResources().getString(R.string.recheck2_phone2));
+
 
         EditText editText3=findViewById(R.id.mail_confirm);
-        editText3.setText("The text3");
+        editText3.setText(getResources().getString(R.string.recheck2_mail1));
+
+        TextView mail_confirm2 = findViewById(R.id.mail_confirm2);
+        mail_confirm2.setText(getResources().getString(R.string.recheck2_mail2));
+
 
         EditText editText4=findViewById(R.id.company_confirm);
-        editText4.setText("The text4");
+        editText4.setText(getResources().getString(R.string.recheck2_company1));
+
+        TextView company_confirm2 = findViewById(R.id.company_confirm2);
+        company_confirm2.setText(getResources().getString(R.string.recheck2_company2));
+
     }
 
 
