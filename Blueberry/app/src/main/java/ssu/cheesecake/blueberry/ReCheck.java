941dacae8872e9d0c750ee87721112296e77fdba
zzarbttoo <qodbwls064@gmail.com>
2019-10-01 08:46:31 +0900

    add intent, need to match array

--- Blueberry/app/src/main/java/ssu/cheesecake/blueberry/ReCheck.java
+++ Blueberry/app/src/main/java/ssu/cheesecake/blueberry/ReCheck.java
@@ -1,6 +1,9 @@
 package ssu.cheesecake.blueberry;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,16 @@ public class ReCheck extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_re_check);
+
+        Button toConfirm_page=findViewById(R.id.finishButton1);
+        toConfirm_page.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent toConfirm_intent=new Intent(ReCheck.this,ReCheck2.class);
+                startActivity(toConfirm_intent);
+                finish();
+            }
+
+        });
     }
 }
