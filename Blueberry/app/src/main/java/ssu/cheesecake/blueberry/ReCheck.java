837ad5dcdd1f209688ba4c18fde33591f44db150
zzarbttoo <qodbwls064@gmail.com>
2019-10-01 08:27:03 +0900

    need to connect

---
+++ Blueberry/app/src/main/java/ssu/cheesecake/blueberry/ReCheck.java
@@ -0,0 +1,14 @@
+package ssu.cheesecake.blueberry;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_re_check);
+    }
+}
