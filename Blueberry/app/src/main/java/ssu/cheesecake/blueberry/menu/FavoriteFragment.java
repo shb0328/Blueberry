06d4d7ceef489fe83a7b21e1e7b15d14f9922882
Hansu Kim <cpuman7@gmail.com>
2019-11-10 13:55:55 +0900

    Finish RecyclerView

--- Blueberry/app/src/main/java/ssu/cheesecake/blueberry/menu/FavoriteFragment.java
+++ Blueberry/app/src/main/java/ssu/cheesecake/blueberry/menu/FavoriteFragment.java
@@ -1,10 +1,14 @@
 package ssu.cheesecake.blueberry.menu;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,7 +16,10 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import ssu.cheesecake.blueberry.R;
 
-public class FavoriteFragment extends Fragment{
+public class FavoriteFragment extends Fragment implements View.OnTouchListener {
+    TextView textView;
+    Animation item_menu_open;
+    float initX;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,7 +30,23 @@ public class FavoriteFragment extends Fragment{
         BottomNavigationView navView = navHostFragment.getActivity().findViewById(R.id.nav_view);
         Menu menu = navView.getMenu();
         menu.getItem(2).setChecked(true);
+
         return root;
     }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            Log.d("EVENT!!", "Down!");
+            initX = event.getRawX();
+        }
+        else if(event.getAction() == MotionEvent.ACTION_MOVE){
+            Log.d("EVENT!!", "Move!");
+            if(initX > event.getRawX()){
+                Log.d("EVENT!!", "MoveIf!");
+                textView.startAnimation(item_menu_open);
+            }
+        }
+        return true;
+    }
 }
