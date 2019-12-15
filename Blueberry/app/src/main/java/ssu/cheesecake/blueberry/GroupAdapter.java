d634bc751650e6bc6f0bbe21f8a7a85f6d3aecde
hyebeen <hyebeen0328@naver.com>
2019-12-15 19:28:32 +0900

    thumbnail 관련 주석 제거

--- Blueberry/app/src/main/java/ssu/cheesecake/blueberry/GroupAdapter.java
+++ Blueberry/app/src/main/java/ssu/cheesecake/blueberry/GroupAdapter.java
@@ -6,15 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.Vector;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import ssu.cheesecake.blueberry.custom.CustomGroup;
-import ssu.cheesecake.blueberry.menu.GroupFragment;
 
 public class GroupAdapter extends ArrayAdapter {
 
@@ -43,16 +40,9 @@ public class GroupAdapter extends ArrayAdapter {
         }
 
         GroupHolder holder = (GroupHolder)convertView.getTag();
-//        ImageView thumbnail = holder.thumbnail;
         TextView groupName = holder.groupName;
 
         CustomGroup customGroup = data.get(position);
-
-//        String url = businessCard.getImageUrl();
-//        Bitmap img = BitmapFactory.decodeFile(url);
-//        thumbnail.setImageBitmap(img);
-
-//        thumbnail.setImageBitmap(img);
 
         String groupname = customGroup.getGroupName();
         groupName.setText(groupname);
--- Blueberry/app/src/main/java/ssu/cheesecake/blueberry/GroupHolder.java
+++ Blueberry/app/src/main/java/ssu/cheesecake/blueberry/GroupHolder.java
@@ -4,12 +4,10 @@ import android.view.View;
 import android.widget.TextView;
 
 public class GroupHolder {
-//    ImageView thumbnail;
     TextView groupName;
 
     public GroupHolder(View view) {
         super();
-//        thumbnail = view.findViewById(R.id.thumbnail);
         groupName = view.findViewById(R.id.groupname);
     }
 }
