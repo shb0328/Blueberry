5616087ac3539a31fbd55d3a10e4714f17813414
Hansu Kim <cpuman7@gmail.com>
2019-12-19 18:41:18 +0900

    edit design 2

--- Blueberry/app/src/main/java/ssu/cheesecake/blueberry/menu/OptionFragment.java
+++ Blueberry/app/src/main/java/ssu/cheesecake/blueberry/menu/OptionFragment.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
